0da176ca129d18fdf04fbf1bdb5597da2a8fa81c
Samer Buna <samerbuna@users.noreply.github.com>
2025-03-30 11:01:45 -0700

    Cleanup example code and simplify to 3-tabs layout

--- app/(tabs)/types.tsx
+++ app/(tabs)/types.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function HomeScreen() {
+export default function TypesScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Types</Text>
     </View>
   );
 }
